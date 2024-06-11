4564d13812472dfed0ae177eeaeca3672987b2dd
DuyChinh <doanduychinh2102@gmail.com>
2024-06-12 06:09:39 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Vue 3 + Vite
+
+# To do App
+## Build todos for 1 days work
+
+
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+import TodoApp from './components/TodoApp.vue'
+import Header from './components/Header.vue';
+</script>
+
+<template>
+  <div>
+    <Header/>
+    <p>Xin chao</p>
+    <TodoApp/>
+  </div>
+</template>
+
+<style scoped>
+</style>
